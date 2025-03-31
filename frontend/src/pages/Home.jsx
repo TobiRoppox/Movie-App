b7c41ad40859266426bb10f8fa60dc2f94fc997a
Clifford Halasan <cliffordhalasan11@gmail.com>
2025-03-31 01:35:41 -0400

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import MovieCard from "../components/MovieCard"
+
+function Home() {
+    const movies = [
+        {id: 1, title: "John Wick", release_date: "2020"},
+        {id: 2, title: "John Wick 2", release_date: "2021"},
+        {id: 3, title: "John Wick 3", release_date: "2022"},
+        {id: 3, title: "John Wick 3", release_date: "2023"},
+    ]
+
+    const handleSearch = () => {};
+    return <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input type="text" placeholder="Search ... " 
+            className="search-input"/>
+        </form>
+        <button type="submit" className="search-button">Search</button>
+
+        <div className="movies-grid">
+            {movies.map((movie) => (
+             <MovieCard movie={movie} key={movie.id}/>
+             ))}
+        </div>
+
+    </div>
+    
+}
+
+export default Home
